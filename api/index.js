95481685701a8ff55feb467df1d284b644f84463
PrudentProgrammer7 <105739240+PrudentProgrammer7@users.noreply.github.com>
2023-05-06 02:37:21 +0000

    WebHook update

--- api/index.js
+++ api/index.js
@@ -12,7 +12,16 @@ app.post("/webhook", (req, res) => {
     let body = req.body;
     res.send('It works');
     
-  
+  // Send a 200 OK response if this is a page webhook
+
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+    //console logs
     console.log(`\u{1F7EA} Received webhook:`);
     console.dir(body, { depth: null });
 });
