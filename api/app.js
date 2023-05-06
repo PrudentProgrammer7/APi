190364a8ca45c2545a956594f5eb26d7f996805f
Jean Bisono <jeanbisono@Jeans-MacBook-Air.local>
2023-05-05 21:23:35 -0400

    Initial commit

---
+++ api/app.js
@@ -0,0 +1,73 @@
+// Require express and body-parser
+const express = require("express")
+const bodyParser = require("body-parser")
+
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btn");
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+  // Clone the wrapper
+  dark = !dark;
+  let clone = big_wrapper.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
+
+  document.body.classList.add("stop-scrolling");
+
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
+}
+
+events();
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.post("/webhook", (req, res) => {
+    let body = req.body;
+  
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+});
+
