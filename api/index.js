6198fde674cb8fc22cf73765e8aac785c6e523a3
PrudentProgrammer7 <105739240+PrudentProgrammer7@users.noreply.github.com>
2023-05-05 21:53:31 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,12 +5,12 @@ const port = 3000
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile('/index.html')
+  res.sendFile('/opt/render/project/src/api/index.html')
 });
 
 app.post("/webhook", (req, res) => {
     let body = req.body;
-    res.send('It works!)
+    
   
     console.log(`\u{1F7EA} Received webhook:`);
     console.dir(body, { depth: null });
