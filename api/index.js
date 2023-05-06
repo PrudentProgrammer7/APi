a53bcf5d7ec3ebd63d0fef845bf7cf3a1695361a
PrudentProgrammer7 <105739240+PrudentProgrammer7@users.noreply.github.com>
2023-05-05 21:42:10 -0400

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile('/Users/jeanbisono/Express/FbWh/api/index.html')
+});
+
+app.post("/webhook", (req, res) => {
+    let body = req.body;
+  
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
